73a7881d2241c1a3acf16fbaba2f482871d07ff1
Marcus <marcushvingelby@gmail.com>
2023-02-01 08:17:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const fs = require('fs')
 
 const bilbasen = require('./websites/bilbasen')
 const dba = require('./websites/dba')
-const Module = require('module')
 
 const userAgent = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'}
 
@@ -32,46 +31,47 @@ class Listing {
 
 
 
-var searchUrl = bilbasen.buildUrl("Corvette", [0, 500000])
+
 
 //searchUrl = dba.buildUrl("corvette", [0, 500000], 100);
 
 
 
 const ScrapeUrl = async function(url) {
+    console.log(url)
 
     try {
-        const { data } = await axios.get(url, {headers: userAgent})
+        const { data } = await axios.get(url, { headers: userAgent } )
         const $ = cheerio.load(data)
 
         const listingUrls = $('.bb-listing-clickable .listing-heading').map((_, listingUrl) => {
             return 'https://bilbasen.dk' + $(listingUrl).attr('href')
         }).get()
 
-        listingUrls.forEach(async listingUrl => {
-            let foundListings = []
-
-            const { data: listingData } = await axios.get(listingUrl, {headers: userAgent})
+        foundListings = new Array()
+        foundListings = await Promise.all(listingUrls.map(async listingUrl => {
+            const { data: listingData } = await axios.get(listingUrl, { headers: userAgent })
             const listingPage = cheerio.load(listingData)
 
             const imageLink = listingPage('#root > div.nmp-ads-layout__page > div.nmp-ads-layout__content > div.bas-Wrapper-wrapper > article > main > div.bas-MuiVipPageComponent-mainGallery > a > div > img').attr('src')
             const price = parseInt(listingPage('.bas-MuiCarPriceComponent-value').text().replace('kr.', '').replace('.', ''))
             const kilometers = parseInt(listingPage('#root > div.nmp-ads-layout__page > div.nmp-ads-layout__content > div.bas-Wrapper-wrapper > article > main > div:nth-child(5) > div > table > tbody > tr:nth-child(3) > td').text().replace('km.', '').replace('.', ''))
             const productionYear = parseInt(listingPage('#root > div.nmp-ads-layout__page > div.nmp-ads-layout__content > div.bas-Wrapper-wrapper > article > main > div:nth-child(5) > div > table > tbody > tr:nth-child(1) > td').text())
+        
+            return new Listing(listingUrl, imageLink, price, kilometers, productionYear, foundListings.length + 1)
+        }))
 
-            let newListing = new Listing(listingUrl, imageLink, price, kilometers, productionYear, foundListings.length + 1)
-            foundListings.push(newListing)
+        console.log(foundListings)
+        foundListings = JSON.stringify(helperFunctions.sortListingsByPrice(foundListings, true), null, 2)
 
-            foundListings = JSON.stringify(helperFunctions.sortListingsByPrice(foundListings, true), null, 2)
-            console.log(foundListings)
-
-            return foundListings;
-        })
+        return foundListings;
+ 
     } catch(error) {
         console.error(error)
     }
 }
 
-//ScrapeUrl(searchUrl)
+var searchUrl = bilbasen.buildUrl("Corvette", [0, 500000])
+ScrapeUrl(searchUrl)
 
 exports.ScrapeUrl = ScrapeUrl
